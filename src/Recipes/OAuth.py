25edecd4cee89b937767b56ba15b3b05d20d0ac1
Ryan Liao <116501169@qq.com>
2016-08-03 23:44:20 +0700

    organize

--- src/Recipes/OAuth.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import twitter
-
-from twitter.oauth import write_token_file, read_token_file
-from twitter.oauth_dance import oauth_dance
-
-
--- src/Recipes/Test_python.py
+++
@@ -1,7 +0,0 @@
-aList = ["a","b","c"]
-print aList
-aList.remove("b")
-print aList
-aList.remove("e")
-aList.append("d")
-print aList
